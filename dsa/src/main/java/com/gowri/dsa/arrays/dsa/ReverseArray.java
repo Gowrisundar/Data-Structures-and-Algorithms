e9bcbd47afc021a7bcac5046b3e5cdd5566eb343
Gowri Sundar Suriyanarayanan <gowrisundar93@gmail.com>
2023-12-29 20:43:43 +0530

    chore: Practise Reverse Array with Test Cases

---
+++ dsa/src/main/java/com/gowri/dsa/arrays/dsa/ReverseArray.java
@@ -0,0 +1,21 @@
+package com.gowri.dsa.arrays.dsa;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class ReverseArray 
+{
+   public static List<Integer> reverse(List<Integer> intList) {
+	   List<Integer> randomList = intList;
+	   int listLength =randomList.size();
+	   for(int i=0, j=listLength-1; i<listLength/2; i++, j--) {
+		   int tmp = randomList.get(i);
+		   randomList.set(i, randomList.get(j));
+		   randomList.set(j, tmp);
+	   }
+	   return randomList;
+   }
+}
---
+++ dsa/src/test/java/com/gowri/dsa/arrays/dsa/ReverseArrayTest.java
@@ -0,0 +1,53 @@
+package com.gowri.dsa.arrays.dsa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for Reverse Array Test
+ * @author gowrisundarsuriyanarayanan
+ */
+public class ReverseArrayTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ReverseArrayTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ReverseArrayTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testReverseArrayEvenSize()
+    {
+    	List<Integer> evenArray = new ArrayList<>(Arrays.asList(1, 3, 4, 5));
+    	List<Integer> expectedArray = new ArrayList<>(Arrays.asList(5, 4, 3, 1));
+    	assertEquals(expectedArray, ReverseArray.reverse(evenArray));
+    }
+    
+    public void testReverseArrayOddSize()
+    {
+    	List<Integer> oddArray = new ArrayList<>(Arrays.asList(9, 5, 7, 6, 3, 1, 8));
+    	List<Integer> expectedArray = new ArrayList<>(Arrays.asList(8, 1, 3, 6, 7, 5, 9));
+    	assertEquals(expectedArray, ReverseArray.reverse(oddArray));
+    }
+}
