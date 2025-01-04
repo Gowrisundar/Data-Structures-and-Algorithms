3a10735508fc36f84a2e5635c81637958d49c2ef
Gowri Sundar Suriyanarayanan <gowrisundar93@gmail.com>
2025-01-05 00:49:50 +0530

    Maximum circular Sub Array Sum GKG and LeetCode

---
+++ GeeksForGeeks160Days/src/com/gowri/dsa/gfg160/arrays/MaximumCircularSubArray.java
@@ -0,0 +1,34 @@
+package com.gowri.dsa.gfg160.arrays;
+
+public class MaximumCircularSubArray {
+	
+    public int maxSubarraySumCircular(int[] nums) {
+        int totalSum = 0;
+        int currMaxSum = 0;
+        int currMinSum = 0;
+        int maxSum = nums[0];
+        int minSum = nums[0];
+
+        for(int i =0; i<nums.length; i++) {
+            currMaxSum = Math.max(currMaxSum + nums[i], nums[i]);
+            maxSum = Math.max(currMaxSum, maxSum);
+
+            currMinSum = Math.min(currMinSum + nums[i], nums[i]);
+            minSum = Math.min(currMinSum, minSum);
+
+            totalSum += nums[i];
+        }
+
+        int normalSum = maxSum;
+
+        if(minSum == totalSum) {
+            return normalSum;
+        }
+        int circularSum =totalSum - minSum;
+
+        return Math.max(normalSum, circularSum);
+
+        
+    }
+
+}
